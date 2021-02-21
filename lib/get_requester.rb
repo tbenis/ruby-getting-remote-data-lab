9869dd59d3afb1b25a649d5436f7d78ed7e19e4a
Benis Tambe <will.jenifer@yahoo.com>
2021-02-20 22:00:27 -0500

    commit tree 75f4d540cc775bfd40c9499786bdbf76cf013eff

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,23 @@
 # Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+
+    def initialize(url)
+        @url = url
+    end
+
+    def get_response_body
+        uri = URI.parse(@url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def parse_json
+        items = JSON.parse(self.get_response_body)
+        items
+    end
+
+end
